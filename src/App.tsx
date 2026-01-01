6f8981d852acebb8624b627d8355bbed0916338d
zuya <kazu.prgrm555y@gmail.com>
2026-01-01 16:20:09 +0900

    refactor: 💡 add routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,21 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Signup from "./pages/Signup";
+import Signin from "./pages/Signin";
+import Home from "./pages/Home";
+import CreateWorkspace from "./pages/CreateWorkspace";
+
 function App() {
   return (
-    <>
-      <div>Hello world</div>
-    </>
+    <BrowserRouter>
+      <div>
+        <Routes>
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/signin" element={<Signin />} />
+          <Route path="/" element={<CreateWorkspace />} />
+          <Route path="/:workspaceId/:channelId" element={<Home />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 

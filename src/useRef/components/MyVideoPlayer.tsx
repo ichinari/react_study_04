7ec01b5a8536b2b94ea63fc5466bc1542df40a7e
kzoo <>
2026-03-01 13:17:26 +0900

    fix: 🐛 update MyVideoPlayer component to accept optional ref prop

--- src/useRef/components/MyVideoPlayer.tsx
+++ src/useRef/components/MyVideoPlayer.tsx
@@ -2,10 +2,14 @@ interface Props {
   src: string;
   type: string;
   width: string;
-  ref: React.RefObject<HTMLVideoElement | null>;
 }
 
-const MyVideoPlayer = ({ src, type, width, ref }: Props) => {
+const MyVideoPlayer = ({
+  src,
+  type,
+  width,
+  ref,
+}: Props & { ref?: React.RefObject<HTMLVideoElement | null> }) => {
   return (
     <video width={width} ref={ref}>
       <source src={src} type={type} />
